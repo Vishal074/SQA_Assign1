80b2cae448b0da207691644f3f68c66377b699fb
Vishal Kapadia <vkapadia4781@consetogac.on.ca>
2019-05-30 12:19:56 -0400

    console application completed

--- sql_assig1/Program.cs
+++ sql_assig1/Program.cs
@@ -10,7 +10,90 @@ namespace sql_assig1
     {
         static void Main(string[] args)
         {
-
+            string strLength, strWidth;
+            int length, width;
+            while (true)
+            {
+                Console.Write("Enter Length: ");
+                strLength = Console.ReadLine();
+                if (!int.TryParse(strLength, out length) || (length <= 0))
+                    Console.Write("\n\tInvalid Input\n\tplease enter again");
+                else
+                    break;
+            }
+            while (true)
+            {
+                Console.Write("Enter Width: ");
+                strWidth = Console.ReadLine();
+                if (!int.TryParse(strWidth, out width) || (width <= 0))
+                    Console.Write("\n\tInvalid Input\n\tplease enter again");
+                else
+                    break;
+            }
+            Rectangle rect = new Rectangle(length, width);
+            int opt = 0;
+            do
+            {
+                while (true)
+                {
+                    Console.Write("\n\t1. Get Rectangle Length\n" +
+                        "\t2. Change Rectangle Length\n" +
+                        "\t3. Get Rectangle Width\n" +
+                        "\t4. Change Rectangle Width\n" +
+                        "\t5. Get Rectangle Perimeter\n" +
+                        "\t6. GetRectangle Area\n" +
+                        "\t7. Exit\nEnter your choise:");
+                    if (!int.TryParse(Console.ReadLine(), out opt) || (opt > 7 || opt < 1))
+                        Console.Write("\n\tInvalid Input\n\tplease enter again");
+                    else
+                        break;
+                }
+                switch (opt)
+                {
+                    case 1:
+                        Console.WriteLine("Length of ractangle: " + rect.GetLength().ToString());
+                        break;
+                    case 2:
+                        while (true)
+                        {
+                            Console.Write("Enter Length: ");
+                            strLength = Console.ReadLine();
+                            if (!int.TryParse(strLength, out length) || (length <= 0))
+                                Console.Write("\n\tInvalid Input\n\tplease enter again");
+                            else
+                                break;
+                        }
+                        rect.SetLength(length);
+                        break;
+                    case 3:
+                        Console.WriteLine("Width of ractangle: " + rect.GetWidth().ToString());
+                        break;
+                    case 4:
+                        while (true)
+                        {
+                            Console.Write("Enter Width: ");
+                            strWidth = Console.ReadLine();
+                            if (!int.TryParse(strWidth, out width) || (width <= 0))
+                                Console.Write("\n\tInvalid Input\n\tplease enter again");
+                            else
+                                break;
+                        }
+                        rect.SetWidth(width);
+                        break;
+                    case 5:
+                        Console.WriteLine("Perimeter of ractangle: " + rect.GetPerimeter().ToString());
+                        break;
+                    case 6:
+                        Console.WriteLine("Area of ractangle: " + rect.GetArea().ToString());
+                        break;
+                    case 7:
+                        Console.WriteLine("Good bye");
+                        break;
+                    default:
+                        Console.WriteLine("Exception occured");
+                        break;
+                }
+            } while (opt != 7);
         }
     }
 }
